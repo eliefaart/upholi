7bd3be813d3034ac3aeed06db80f3431ef26193c
Eric Liefaart <eliefaart@sector-orange.com>
2020-04-20 14:04:31 +0200

    Add some tests; testing how Github Workflows react

--- src/main.rs
+++ src/main.rs
@@ -3,3 +3,19 @@
 fn main() {
 	println!("Hello world.");
 }
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(1, 1);
+    }
+
+    #[test]
+    fn test_bad_add() {
+        assert_eq!(1, 3);
+    }
+}
---
+++ tests/integration_tests.rs
@@ -0,0 +1,9 @@
+#[test]
+fn integration_test_add() {
+    assert_eq!(1, 1);
+}
+
+#[test]
+fn integration_test_bad() {
+    assert_eq!(1, 2);
+}
