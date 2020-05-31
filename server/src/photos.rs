c49975ad00575a0c0aff939aa332e526ca2f2545
Eric <eric@e10t.nl>
2020-05-31 17:35:40 +0200

    Finalize implementation of EXIF parser

--- server/src/photos.rs
+++ server/src/photos.rs
@@ -8,6 +8,7 @@ use crate::files;
 use crate::database;
 use crate::types;
 use crate::ids;
+use crate::exif::Exif;
 
 const DIMENSIONS_THUMB: u32 = 400;
 const DIMENSIONS_PREVIEW: u32 = 1500;
@@ -25,27 +26,6 @@ pub struct Photo {
 	pub path_preview: String,
 	pub path_original: String,
 	pub exif: Exif
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-#[serde(rename_all = "camelCase")]
-pub struct Exif {
-	pub manufactorer: Option<String>,
-	pub model: Option<String>,
-	pub aperture: Option<String>,
-	pub exposure_time: Option<String>,
-	pub iso: Option<i32>,
-	pub focal_length: Option<i32>,
-	pub focal_length_35mm_equiv: Option<i32>,
-	pub orientation: Option<i32>,
-	pub date_taken: Option<String>
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-#[serde(rename_all = "camelCase")]
-pub struct ExifEntrySlim {
-	pub ifd_tag: u16,
-	pub value_readable: String
 }
 
 impl Photo {
@@ -74,7 +54,7 @@ impl Photo {
 		
 			let (width, height) = images::get_image_dimensions(photo_bytes);
 		
-			let exif = Self::parse_exif(photo_bytes)?;
+			let exif = Exif::parse_from_photo_bytes(photo_bytes)?;
 
 			println!("{:?}", exif);
 
@@ -142,64 +122,6 @@ impl Photo {
 		
 		// Concat
 		Ok(format!("{}{}", name, extension))
-	}
-
-	fn parse_exif(photo_bytes: &Vec<u8>) -> Result<Exif, String> {
-
-		let result = rexif::parse_buffer(photo_bytes);
-		match result {
-			Ok(exif) => {
-
-				let closure_get_exif_data_as_string = |tag: rexif::ExifTag| -> Option<String> {
-					let result = exif.entries.iter().find(|entry| entry.tag == tag);
-					if let Some(entry) = result {
-						match &entry.value {
-							rexif::TagValue::Ascii(val) => Some(val.to_string()),
-							rexif::TagValue::URational(_) => Some(entry.value_more_readable.to_string()),
-							_ => {
-								println!("{:?}", entry.value);
-								None
-							}
-						}
-					} else {
-						None
-					}
-				};
-
-				let closure_get_exif_data_as_i32 = |tag: rexif::ExifTag| -> Option<i32> {
-					let result = exif.entries.iter().find(|entry| entry.tag == tag);
-					if let Some(entry) = result {
-						match &entry.value {
-							rexif::TagValue::U32(val) => Some(val[0] as i32),
-							rexif::TagValue::U16(val) => Some(val[0] as i32),
-							rexif::TagValue::U8(val) => Some(val[0] as i32),
-							rexif::TagValue::I32(val) => Some(val[0] as i32),
-							rexif::TagValue::I16(val) => Some(val[0] as i32),
-							rexif::TagValue::I8(val) => Some(val[0] as i32),
-							_ => {
-								println!("{:?}", entry.value);
-								None
-							}
-						}
-					} else {
-						None
-					}
-				};
-
-				Ok(Exif {
-					manufactorer: closure_get_exif_data_as_string(rexif::ExifTag::Make),
-					model: closure_get_exif_data_as_string(rexif::ExifTag::Model),
-					aperture: closure_get_exif_data_as_string(rexif::ExifTag::FNumber),
-					exposure_time: closure_get_exif_data_as_string(rexif::ExifTag::ExposureTime),
-					iso: closure_get_exif_data_as_i32(rexif::ExifTag::ISOSpeedRatings),
-					focal_length: closure_get_exif_data_as_i32(rexif::ExifTag::FocalLength),
-					focal_length_35mm_equiv: closure_get_exif_data_as_i32(rexif::ExifTag::FocalLengthIn35mmFilm),
-					orientation: closure_get_exif_data_as_i32(rexif::ExifTag::Orientation),
-					date_taken: closure_get_exif_data_as_string(rexif::ExifTag::DateTime)
-				})
-			},
-			Err(error) => Err(format!("{:?}", error))
-		}
 	}
 }
 
