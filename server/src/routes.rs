ac26ce9f340d2c13e6e936f9ad31ad166d9fc584
Eric <eric@e10t.nl>
2020-06-12 22:59:40 +0200

    Replace panics in route handlers with proper HTTP error responses (400, 404, etc)

--- server/src/routes.rs
+++ server/src/routes.rs
@@ -1,6 +1,4 @@
-use http::{StatusCode};
 use actix_web::{web, HttpRequest, HttpResponse, Responder};
-//use actix_http::Response;
 use actix_multipart::{Multipart, Field};
 use serde::{Serialize, Deserialize};
 use futures::{StreamExt, TryStreamExt};
@@ -29,7 +27,7 @@ struct FormData {
 }
 
 pub async fn index() -> impl Responder {
-	format!("Hello world")
+	format!("Service running")
 }
 
 pub async fn route_get_albums() -> impl Responder {
@@ -75,9 +73,9 @@ pub async fn route_get_album(req: HttpRequest) -> impl Responder {
 					}
 				}
 			};
-			web::Json(response)
+			HttpResponse::Ok().json(response)
 		},
-		None => panic!("no album") // How to return HTTP 404?
+		None => create_not_found_response()
 	}
 }
 
@@ -87,8 +85,8 @@ pub async fn route_create_album(album: web::Json<albums::Album>) -> impl Respond
 	let result = database::album::insert(&album);
 
 	match result {
-		Ok(_) => web::Json(CreatedResult{id: album.id}),
-		Err(error) => panic!(error)
+		Ok(_) => HttpResponse::Ok().json(CreatedResult{id: album.id}),
+		Err(error) => create_internal_server_error_response(&error)
 	}
 }
 
@@ -99,8 +97,8 @@ pub async fn route_update_album(req: HttpRequest, album: web::Json<types::Update
 
 	let result = database::album::update(&album_id, &album);
 	match result {
-		Some(_) => HttpResponse::build(StatusCode::OK),
-		None => HttpResponse::build(StatusCode::NOT_FOUND)
+		Some(_) => create_ok_response(),
+		None => create_not_found_response()
 	}
 }
 
@@ -109,8 +107,8 @@ pub async fn route_delete_album(req: HttpRequest) -> impl Responder {
 	let result = database::album::delete(&album_id);
 
 	match result {
-		Some(_) => HttpResponse::build(StatusCode::OK),
-		None => HttpResponse::build(StatusCode::NOT_FOUND)
+		Some(_) => create_ok_response(),
+		None => create_not_found_response()
 	}
 }
 
@@ -118,23 +116,22 @@ pub async fn route_get_photos() -> impl Responder {
 	web::Json(database::photo::get_all())
 }
 
+
+/// Delete a single photo
+pub async fn route_delete_photo(req: HttpRequest) -> impl Responder {
+	let photo_id = req.match_info().get("photo_id").unwrap();
+
+	delete_photos(&[photo_id])
+}
+
+/// Delete multiple photos
 pub async fn route_delete_photos(photo_ids: web::Json<Vec<String>>) -> impl Responder {
 	let mut ids: Vec<&str> = Vec::new();
-
 	for id in photo_ids.iter() {
 		ids.push(&id);
-
-		// Delete physical files for photo
-		delete_photo_files(&id);
 	}
 
-	// Delete everything from database in one batch
-	let result = database::photo::delete_many(&ids);
-
-	match result {
-		Some(_) => HttpResponse::build(StatusCode::OK),
-		None => HttpResponse::build(StatusCode::NOT_FOUND)
-	}
+	delete_photos(&ids)
 }
 
 pub async fn route_get_photo(req: HttpRequest) -> impl Responder {
@@ -142,8 +139,8 @@ pub async fn route_get_photo(req: HttpRequest) -> impl Responder {
 	let result = database::photo::get(photo_id);
 
 	match result {
-		Some(photo) => web::Json(photo),
-		None => panic!("no photo") // How to return HTTP 404, if 'good' path returns json?
+		Some(photo) => HttpResponse::Ok().json(photo),
+		None => create_not_found_response()
 	}
 }
 
@@ -153,7 +150,7 @@ pub async fn route_download_photo_thumbnail(req: HttpRequest) -> impl Responder 
 
 	match result {
 		Some(photo_info) => serve_photo(&photo_info.path_thumbnail, &photo_info.name),
-		None => panic!("File not found")
+		None => create_not_found_response()
 	}
 }
 
@@ -163,7 +160,7 @@ pub async fn route_download_photo_preview(req: HttpRequest) -> impl Responder {
 
 	match result {
 		Some(photo_info) => serve_photo(&photo_info.path_preview, &photo_info.name),
-		None => panic!("File not found")
+		None => create_not_found_response()
 	}
 }
 
@@ -173,11 +170,12 @@ pub async fn route_download_photo_original(req: HttpRequest) -> impl Responder {
 
 	match result {
 	Some(photo_info) => serve_photo(&photo_info.path_original, &photo_info.name),
-		None => panic!("File not found")
+		None => create_not_found_response()
 	}
 }
 
-fn serve_photo(path: &str, filename: &str) -> impl Responder {
+/// Create an HTTP response that returns a file from disk
+fn serve_photo(path: &str, filename: &str) -> actix_http::Response {
 	let result = files::get_photo(path);
 
 	match result {
@@ -187,7 +185,7 @@ fn serve_photo(path: &str, filename: &str) -> impl Responder {
 				.header(http::header::CONTENT_DISPOSITION, format!("attachment; filename=\"{}\"", filename))
 				.body(file_bytes)
 		},
-		None => panic!("Error reading file content from disk, or file not found")
+		None => create_internal_server_error_response("Error reading file content from disk, or file not found")
 	}
 }
 
@@ -199,7 +197,7 @@ pub async fn route_upload_photo(payload: Multipart) -> impl Responder {
 	let remaining_files = files_iter.count();
 
 	if remaining_files > 0 {
-		return HttpResponse::BadRequest().json(ErrorResult{message: format!("Request contains more than one file.")});
+		return create_bad_request_response("Request contains more than one file.");
 	}
 
 	match file_option {
@@ -210,35 +208,13 @@ pub async fn route_upload_photo(payload: Multipart) -> impl Responder {
 					let result = database::photo::insert(&photo);
 					match result {
 						Ok(_) => HttpResponse::Ok().json(CreatedResult{id: photo.id}),
-						Err(error) => HttpResponse::BadRequest().json(ErrorResult{message: error})
+						Err(error) => create_bad_request_response(&error)
 					}
 				},
-				Err(error) => HttpResponse::BadRequest().json(ErrorResult{message: error})
+				Err(error) => create_bad_request_response(&error)
 			}
 		},
-		None => HttpResponse::BadRequest().json(ErrorResult{message: format!("Request contains no file.")})
-	}
-}
-
-pub async fn route_delete_photo(req: HttpRequest) -> impl Responder {
-	let photo_id = req.match_info().get("photo_id").unwrap();
-
-	delete_photo_files(&photo_id);
-
-	let result = database::photo::delete(&photo_id);
-	match result {
-		Some(_) => HttpResponse::build(StatusCode::OK),
-		None => HttpResponse::build(StatusCode::NOT_FOUND)
-	}
-}
-
-/// Deletes all physical files of a photo from file system
-/// Original, thumbnail and preview images.
-fn delete_photo_files(photo_id: &str) {
-	if let Some(photo) = database::photo::get(&photo_id) {
-		files::delete_photo(&photo.path_original);
-		files::delete_photo(&photo.path_preview);
-		files::delete_photo(&photo.path_thumbnail);
+		None => create_bad_request_response("Request contains no file.")
 	}
 }
 
@@ -275,4 +251,47 @@ async fn get_form_field_bytes(mut field: Field) -> Vec<u8> {
 	}
 
 	field_bytes
+}
+
+/// Delete multiple photos from database and disk
+fn delete_photos(ids: &[&str]) -> impl Responder {
+	// Delete physical files for photo
+	for id in ids {
+		delete_photo_files(&id);
+	}
+	
+	match database::photo::delete_many(ids) {
+		Ok(_) => create_ok_response(),
+		Err(_) => create_not_found_response()
+	}
+}
+
+/// Deletes all physical files of a photo from file system
+/// Original, thumbnail and preview images.
+fn delete_photo_files(photo_id: &str) {
+	if let Some(photo) = database::photo::get(&photo_id) {
+		files::delete_photo(&photo.path_original);
+		files::delete_photo(&photo.path_preview);
+		files::delete_photo(&photo.path_thumbnail);
+	}
+}
+
+/// Create a HTTP 200 OK response
+fn create_ok_response() -> actix_http::Response {
+	HttpResponse::Ok().finish()
+}
+
+/// Create a HTTP 404 Not Found response
+fn create_not_found_response() -> actix_http::Response {
+	HttpResponse::NotFound().finish()
+}
+
+/// Create a HTTP 400 Bad Request response
+fn create_bad_request_response(message: &str) -> actix_http::Response {
+	HttpResponse::BadRequest().json(ErrorResult{message: message.to_string()})
+}
+
+/// Create a HTTP 500 Internal Server Error response
+fn create_internal_server_error_response(message: &str) -> actix_http::Response {
+	HttpResponse::InternalServerError().json(ErrorResult{message: message.to_string()})
 }
