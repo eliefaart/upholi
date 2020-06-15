1024e27530e4913273a78f348816e220506bbcb3
Eric Liefaart <eric@e10t.nl>
2020-06-15 21:59:46 +0200

    Implement oauth client functionality (#13)
    Add routes to authorize a user via an oauth identity provider.
    All /api routes now require a session cookie to exist. Content of this session cookie is not finished yet.
    Frontend will initiate login flow if there is no user.

--- server/src/settings.rs
+++ server/src/settings.rs
@@ -3,29 +3,38 @@ use std::env::{var};
 const ENV_VAR_DB_CONNECTION_STRING: &str = "HB_DB_CONNSTRING";
 const ENV_VAR_DB_NAME: &str = "HB_DB_NAME";
 const ENV_VAR_DIR_PHOTOS: &str = "HB_DIR_PHOTOS";
+const ENV_VAR_OAUTH_CLIENT_ID: &str = "HB_OAUTH_CLIENTID";
+const ENV_VAR_OAUTH_CLIENT_SECRET: &str = "HB_OAUTH_CLIENTSECRET";
 
 /// Application settings
-#[derive(Debug)]
 pub struct Settings {
 	pub database: Database,
-	pub photos: Photos
+	pub photos: Photos,
+	pub oauth: OAuth
 }
 
 /// Database settings
-#[derive(Debug)]
 pub struct Database {
 	pub connection_string: String,
 	pub name: String
 }
 
 /// Photos settings
-#[derive(Debug)]
 pub struct Photos {
 	pub base_directory: String
 }
 
+/// OAuth setting of identity provider
+pub struct OAuth {
+	pub client_id: String,
+	pub client_secret: String,
+	pub auth_url: String,
+	pub token_url: String,
+	pub userinfo_url: String
+}
+
 impl Default for Settings {
-    fn default() -> Self {
+	fn default() -> Self {
 		Self::new()
 	}
 }
@@ -33,7 +42,7 @@ impl Default for Settings {
 impl Settings {
 	/// Get all application settings
 	/// It just panics now if something went wrong (eg missing env var)
-    pub fn new() -> Self {
+	pub fn new() -> Self {
 		Self {
 			database: Database{
 				connection_string: Self::get_env_var(ENV_VAR_DB_CONNECTION_STRING),
@@ -41,6 +50,13 @@ impl Settings {
 			},
 			photos: Photos{
 				base_directory: Self::get_env_var(ENV_VAR_DIR_PHOTOS)
+			},
+			oauth: OAuth{
+				client_id: Self::get_env_var(ENV_VAR_OAUTH_CLIENT_ID),
+				client_secret: Self::get_env_var(ENV_VAR_OAUTH_CLIENT_SECRET),
+				auth_url: "https://github.com/login/oauth/authorize".to_string(),
+				token_url: "https://github.com/login/oauth/access_token".to_string(),
+				userinfo_url: "https://api.github.com/user".to_string(),
 			}
 		}
 	}
