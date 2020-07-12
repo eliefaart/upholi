9b65c38abd1b5f0f0f2d10f5fa9cad79e97be86d
Eric <eric@e10t.nl>
2020-07-12 21:17:12 +0200

    Allow photo zooming + panning by mouse

--- app/src/components/PhotoDetail.jsx
+++ app/src/components/PhotoDetail.jsx
@@ -4,28 +4,180 @@ import ExifData from '../components/ExifData.jsx';
 class PhotoDetail extends React.Component {
 	constructor(props) {
 		super(props);
+
+		this.state = {
+			isPanning: false
+		};
 	}
 
 	componentDidMount() {
-		// TODO: Center the photo displayed.
-		// EITHER center photo within iframe, but struggling with that. I can center it, but then I can no longer zoom.
-		// OR center iframe within parent, then I need to implement iframe sending messages to parent about what size it wants to be.
+		const imgElement = document.getElementsByClassName("photoDetailImg")[0];
 
-		// window.frames[0].document.body.onload = () => {
-		// 	// Apply some styling to the elements inside the iframe, to get rid of unwanted margins etc.
+		// Zoom on mousewheel
+		imgElement.onwheel = (event) => {
+			this.zoomPhoto(imgElement, event.deltaY);
+		};
 
-		// 	const iframeBody = window.frames[0].document.body;
-		// 	iframeBody.style.margin = "0px";
+		// Pan photo on mouse or touch move
+		let panLastX, panLastY;
+		const fnStartPanning = (event) => {
+			this.setState({isPanning: true });
+			panLastX = event.clientX;
+			panLastY = event.clientY;
+		};
+		const fnStopPanning = () => this.setState({isPanning: false });
+		const fnOnMouseMove = (event) => {
+			if (this.state.isPanning) {
+				const currentX = event.clientX;
+				const currentY = event.clientY;
 
-		// 	// const imgElement = iframeBody.getElementsByTagName("img")[0];
-		// };
+				let deltaX = currentX - panLastX;
+				let deltaY = currentY - panLastY;
+
+				this.movePhoto(imgElement, deltaX, deltaY);
+
+				panLastX = currentX;
+				panLastY = currentY;
+			}
+		};
+
+		imgElement.onmousedown = fnStartPanning;
+		imgElement.ontouchstart = fnStartPanning;
+		imgElement.onmouseup = fnStopPanning;
+		imgElement.onmouseleave = fnStopPanning;
+		imgElement.ontouchend = fnStopPanning;
+		imgElement.onmousemove = fnOnMouseMove;
+	}
+
+	// Zoom the image by given number of units
+	zoomPhoto(imgElement, zoomDelta) {
+		const zoomStepPercentage = 10;
+		const zoomingIn = zoomDelta < 0;
+
+		// Find current scale factor
+		let matches = /scale\((.+)\)/.exec(imgElement.style.transform);
+		const currentScaleFactor = matches && matches.length >= 2
+			? parseFloat(matches[1])
+			: 1;
+
+		// Calculate new scale factor
+		const zoomStep = (currentScaleFactor / 100) * zoomStepPercentage;
+		let newScaleFactor = zoomingIn 
+			? currentScaleFactor + zoomStep 
+			: currentScaleFactor - zoomStep;
+
+		// Set new scale factor
+		imgElement.style.transform = "scale(" + newScaleFactor + ")";
+	}
+
+	// Move/pan the image by given number of units.
+	movePhoto(imgElement, deltaX, deltaY) {
+		const imgWidth = imgElement.getBoundingClientRect().width;
+		const imgHeight = imgElement.getBoundingClientRect().height;
+		const containerWidth = imgElement.parentElement.getBoundingClientRect().width;
+		const containerHeight = imgElement.parentElement.getBoundingClientRect().height;
+
+		const imgFitsInContainerX = imgWidth <= containerWidth;
+		const imgFitsInContainerY = imgHeight <= containerHeight;
+		
+		// Image smaller than container, X-axis
+		if (imgFitsInContainerX && deltaX !== 0) {
+			let movingRight = deltaX > 0;
+			let movingLeft = !movingRight;
+
+			if (movingRight) {
+				const availablePixels = -(imgElement.getBoundingClientRect().right - imgElement.parentElement.getBoundingClientRect().width);
+				
+				if (deltaX > availablePixels) {
+					deltaX = availablePixels;
+				}
+			}
+			if (movingLeft) {
+				const availablePixels = imgElement.getBoundingClientRect().left;
+				
+				if (-deltaX > availablePixels) {
+					deltaX = -availablePixels;
+				}
+			}
+		}
+
+		// Image smaller than container, Y-axis
+		if (imgFitsInContainerY && deltaY !== 0) {
+			let movingDown = deltaY > 0;
+			let movingUp = !movingDown;
+
+			if (movingDown) {
+				const availablePixels = -(imgElement.getBoundingClientRect().bottom - imgElement.parentElement.getBoundingClientRect().height);
+				
+				if (deltaY > availablePixels) {
+					deltaY = availablePixels;
+				}
+			}
+			if (movingUp) {
+				const availablePixels = imgElement.getBoundingClientRect().top;
+				
+				if (-deltaY > availablePixels) {
+					deltaY = -availablePixels;
+				}
+			}
+		}
+
+		// Image larger than container, X-axis
+		if (!imgFitsInContainerX && deltaX !== 0) {
+			let movingRight = deltaX > 0;
+			let movingLeft = !movingRight;
+
+			if (movingRight) {
+				const availablePixels = -imgElement.getBoundingClientRect().left;
+				
+				if (deltaX > availablePixels) {
+					deltaX = availablePixels;
+				}
+			}
+			if (movingLeft) {
+				const availablePixels = -(imgElement.parentElement.getBoundingClientRect().width - imgElement.getBoundingClientRect().right);
+				
+				if (-deltaX > availablePixels) {
+					deltaX = -availablePixels;
+				}
+			}
+		}
+
+		// Image larger than container, Y-axis
+		if (!imgFitsInContainerX && deltaY !== 0) {
+			let movingDown = deltaY > 0;
+			let movingUp = !movingDown;
+
+			if (movingDown) {
+				const availablePixels = -imgElement.getBoundingClientRect().top;
+				
+				if (deltaY > availablePixels) {
+					deltaY = availablePixels;
+				}
+			}
+			if (movingUp) {
+				const availablePixels = -(imgElement.parentElement.getBoundingClientRect().height - imgElement.getBoundingClientRect().bottom);
+				
+				if (-deltaY > availablePixels) {
+					deltaY = -availablePixels;
+				}
+			}
+		}
+
+		// Get current top and left values
+		const currentTop = parseFloat(imgElement.style.top.replace("px", ""));
+		const currentLeft = parseFloat(imgElement.style.left.replace("px", ""));
+
+		// Set new top and left values
+		imgElement.style.left = (currentLeft + deltaX) + "px";
+		imgElement.style.top = (currentTop + deltaY) + "px";
 	}
 
 	render() {
 		return <div className="photoDetail">
-			{this.props.exif && <ExifData exif={this.props.exif}/>}
-			{/* <iframe src={this.props.src}/> */}
-			<img src={this.props.src}/>
+			{this.props.exif && !this.state.isPanning && <ExifData exif={this.props.exif}/>}
+			<img className="photoDetailImg" src={this.props.src} draggable={false}
+				style={{top: "0px", left: "0px"}}/>
 		</div>;
 	}
 }
