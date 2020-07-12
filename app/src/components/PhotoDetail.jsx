a46ccdf9847cc518c9cff47ccbe4c7a1c1f650ef
Eric <eric@e10t.nl>
2020-07-12 09:28:36 +0200

    Revert back to using img to display photos

--- app/src/components/PhotoDetail.jsx
+++ app/src/components/PhotoDetail.jsx
@@ -11,20 +11,21 @@ class PhotoDetail extends React.Component {
 		// EITHER center photo within iframe, but struggling with that. I can center it, but then I can no longer zoom.
 		// OR center iframe within parent, then I need to implement iframe sending messages to parent about what size it wants to be.
 
-		window.frames[0].document.body.onload = () => {
-			// Apply some styling to the elements inside the iframe, to get rid of unwanted margins etc.
+		// window.frames[0].document.body.onload = () => {
+		// 	// Apply some styling to the elements inside the iframe, to get rid of unwanted margins etc.
 
-			const iframeBody = window.frames[0].document.body;
-			iframeBody.style.margin = "0px";
+		// 	const iframeBody = window.frames[0].document.body;
+		// 	iframeBody.style.margin = "0px";
 
-			// const imgElement = iframeBody.getElementsByTagName("img")[0];
-		};
+		// 	// const imgElement = iframeBody.getElementsByTagName("img")[0];
+		// };
 	}
 
 	render() {
 		return <div className="photoDetail">
 			{this.props.exif && <ExifData exif={this.props.exif}/>}
-			<iframe src={this.props.src}/>
+			{/* <iframe src={this.props.src}/> */}
+			<img src={this.props.src}/>
 		</div>;
 	}
 }
