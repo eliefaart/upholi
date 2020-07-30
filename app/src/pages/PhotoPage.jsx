3be7430dba342e6e8e57af4b5b88cbf69d9429d1
Eric Liefaart <eric@e10t.nl>
2020-07-30 22:08:17 +0200

    Remove PhotoBasePage.jsx

--- app/src/pages/PhotoPage.jsx
+++ app/src/pages/PhotoPage.jsx
@@ -1,13 +1,29 @@
 import React from "react";
 import PhotoService from "../services/PhotoService";
+import PhotoDetail from "../components/PhotoDetail.jsx";
+import PageLayout from "../components/PageLayout.jsx"
 import AppStateContext from "../contexts/AppStateContext.jsx";
-import PhotoBasePage from "./PhotoBasePage.jsx"
+import { IconClose, IconDownload } from "../components/Icons.jsx";
 
 class PhotoPage extends React.Component {
-
 	constructor(props) {
 		super(props);
 		this.photoId = props.match.params.photoId;
+
+		this.state = {
+			url: this.props.previewUrl,
+			downloadUrl: this.props.downloadUrl
+		};
+	}
+
+	componentDidMount() {
+		let fnOnPhotoDataReceived = (photo) => {
+			this.setState({ photo });
+		};
+
+		PhotoService.getPhotoInfo(this.props.infoUrl)
+			.then(fnOnPhotoDataReceived)
+			.catch(console.error);
 	}
 
 	render() {
@@ -15,13 +31,19 @@ class PhotoPage extends React.Component {
 		const previewUrl = infoUrl + "/preview";
 		const downloadUrl = infoUrl + "/original";
 
+		const headerActions = (<div>
+			{<a className="iconOnly asButton" href={downloadUrl} download title="Download">
+				<IconDownload/>
+			</a>}
+			{<button className="iconOnly" onClick={() => this.context.history.goBack()} title="Close">
+				<IconClose/>
+			</button>}
+		</div>);
+
 		return (
-			<PhotoBasePage
-				infoUrl={infoUrl}
-				previewUrl={previewUrl}
-				downloadUrl={downloadUrl}
-				requiresAuthentication={false}
-				/>
+			<PageLayout requiresAuthentication={this.props.requiresAuthentication} renderMenu={false} headerActions={headerActions}>
+				<PhotoDetail src={previewUrl} exif={!!this.state.photo ? this.state.photo.exif : null} />
+			</PageLayout>
 		);
 	}
 }
