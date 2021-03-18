090c9fc29ea3232c11092d70f8fb29881ba3fd4a
Eric Liefaart <eric@e10t.nl>
2021-03-18 23:01:46 +0100

    Progress; ass UserCollection.tsx

---
+++ app/src/components/UserCollection.tsx
@@ -0,0 +1,182 @@
+import * as React from "react";
+import AlbumInfo from "../models/AlbumInfo";
+import Collection from "../models/Collection";
+import PhotoService from "../services/PhotoService";
+import CollectionSharingSettings from "./CollectionSharingSettings";
+import { IconChevronDown, IconChevronUp, IconClose, IconCreate, IconDelete } from "./Icons";
+import ModalAddAlbumToCollection from "./modals/ModalAddAlbumToCollection";
+import ModalConfirmation from "./modals/ModalConfirmation";
+import OrderableContent from "./OrderableContent";
+
+interface Props {
+	collection: Collection;
+	onCollectionUpdated: () => void;
+	onCollectionDeleted: () => void;
+}
+
+interface State {
+	settingsOpened: boolean;
+	confirmDeleteCollectionOpen: boolean;
+	confirmRemoveAlbumFromCollectionOpen: boolean;
+	addAlbumToCollectionDialogOpen: boolean;
+	activeAlbum: AlbumInfo | null;
+}
+
+export default class UserCollection extends React.Component<Props, State> {
+	constructor(props: Props) {
+		super(props);
+
+		this.toggleSettings = this.toggleSettings.bind(this);
+		this.onClickDeleteCollection = this.onClickDeleteCollection.bind(this);
+		this.onClickAddAlbumToCollection = this.onClickAddAlbumToCollection.bind(this);
+
+		this.state = {
+			settingsOpened: false,
+			confirmDeleteCollectionOpen: false,
+			confirmRemoveAlbumFromCollectionOpen: false,
+			addAlbumToCollectionDialogOpen: false,
+			activeAlbum: null
+		};
+	}
+
+	/**
+	 * Toggles the settings pane of the collection
+	 */
+	toggleSettings() {
+		this.setState(prevState => {
+			return {
+				settingsOpened: !prevState.settingsOpened
+			};
+		});
+	}
+
+	onClickDeleteCollection() {
+		this.setState({confirmDeleteCollectionOpen: true});
+	}
+
+	onClickRemoveAlbumFromCollection(album: AlbumInfo) {
+		this.setState({
+			confirmRemoveAlbumFromCollectionOpen: true,
+			activeAlbum: album
+		});
+	}
+
+	onClickAddAlbumToCollection() {
+		this.setState({
+			addAlbumToCollectionDialogOpen: true
+		});
+	}
+
+	/**
+	 * Delete the current collection
+	 */
+	deleteCollection() {
+		PhotoService.deleteCollection(this.props.collection.id)
+			.then(() => this.props.onCollectionDeleted())
+			.catch(console.error)
+			.finally(() => this.setState({confirmDeleteCollectionOpen: false}));
+	}
+
+	/**
+	 * Remove an album from the collection
+	 */
+	removeAlbumFromCollection(albumId: string) {
+		PhotoService.removeAlbumFromCollection(this.props.collection.id, albumId)
+			.then(() => this.props.onCollectionUpdated())
+			.catch(console.error)
+			.finally(() => this.setState({confirmRemoveAlbumFromCollectionOpen: false}));
+	}
+
+	/**
+	 * Add an album to the collection
+	 * @param albumId
+	 */
+	addAlbumToCollection(albumId: string) {
+		PhotoService.addAlbumToCollection(this.props.collection.id, albumId)
+			.then(() => this.props.onCollectionUpdated())
+			.catch(console.error)
+			.finally(() => this.setState({addAlbumToCollectionDialogOpen: false}));
+	}
+
+	render() {
+		return <div key={this.props.collection.id} className="collection">
+			<div className="head">
+				{/* Collection title and some actions/buttons */}
+				<h2 className="title">{this.props.collection.title}</h2>
+				<button className="iconOnly" onClick={this.toggleSettings} title="Collection sharing options">
+					{this.state.settingsOpened && <IconChevronUp/>}
+					{!this.state.settingsOpened && <IconChevronDown/>}
+				</button>
+				<button className="iconOnly"
+					onClick={this.onClickDeleteCollection}
+					title="Delete collection">
+					<IconDelete/>
+				</button>
+			</div>
+
+			<div className="body">
+				<div className={"settings" + (this.state.settingsOpened ? " open" : "")}>
+					{this.state.settingsOpened && <CollectionSharingSettings collection={this.props.collection} onOptionsChanged={() => this.props.onCollectionUpdated()}/>}
+				</div>
+				{this.state.settingsOpened && <hr/>}
+
+				{/* Albums inside this collection */}
+				<div className="">
+					<OrderableContent
+						className="collection-albums"
+						onOrderChanged={() => {}}>
+						{this.props.collection.albums.map(album => {
+							let albumThumbUrl = album.thumbPhotoId
+								? "url('" + PhotoService.baseUrl() + "/photo/" + album.thumbPhotoId + "/thumb')"
+								: "";
+
+							return (<div key={album.id}
+								className="album"
+								style={{ backgroundImage: albumThumbUrl }}>
+								<div className="albumContent">
+									<span className="title">{album.title}</span>
+									<button className="iconOnly" onClick={(event) => {
+										event.stopPropagation();
+										this.onClickRemoveAlbumFromCollection(album);
+									}} title="Remove album from collection">
+										<IconClose/>
+									</button>
+								</div>
+							</div>);
+						})}
+					</OrderableContent>
+					<button className="iconOnly" onClick={this.onClickAddAlbumToCollection} title="Add album to collection">
+						<IconCreate/> TODO
+					</button>
+				</div>
+			</div>
+
+
+			{this.state.addAlbumToCollectionDialogOpen && <ModalAddAlbumToCollection
+				isOpen={true}
+				onRequestClose={() => this.setState({addAlbumToCollectionDialogOpen: false})}
+				onAlbumSelected={(album) => this.addAlbumToCollection(album.id)}
+				/>}
+			{this.state.confirmDeleteCollectionOpen && <ModalConfirmation
+				title="Delete collection"
+				isOpen={true}
+				onRequestClose={() => this.setState({confirmDeleteCollectionOpen: false, activeAlbum: null})}
+				onOkButtonClick={() => this.deleteCollection()}
+				okButtonText="Delete"
+				confirmationText={"Collection '" + this.props.collection.title + "' will be deleted."}
+				/>}
+
+			{this.state.confirmRemoveAlbumFromCollectionOpen && <ModalConfirmation
+				title="Remove album from collection"
+				isOpen={true}
+				onRequestClose={() => this.setState({confirmRemoveAlbumFromCollectionOpen: false, activeAlbum: null})}
+				onOkButtonClick={() => this.removeAlbumFromCollection(this.state.activeAlbum!.id)}
+				okButtonText="Remove"
+				confirmationText={"Album '"
+					+ this.state.activeAlbum!.title
+					+ "' will be remove from collection '"
+					+ this.props.collection.title + "'."}
+				/>}
+		</div>;
+	}
+}
--- app/src/components/pages/SharedPage.tsx
+++ app/src/components/pages/SharedPage.tsx
@@ -3,25 +3,13 @@ import { PageBaseComponent, PageBaseComponentProps } from "./PageBaseComponent";
 import ContentContainer from "../ContentContainer"
 import AppStateContext from "../../contexts/AppStateContext";
 import ModalCreateCollection from "../modals/ModalCreateCollection"
-import ModalAddAlbumToCollection from "../modals/ModalAddAlbumToCollection"
-import ModalConfirmation from "../modals/ModalConfirmation"
-import { IconCreate, IconDelete, IconShare, IconClose, IconChevronDown, IconChevronUp } from "../Icons";
 import PhotoService from "../../services/PhotoService";
 import Collection from "../../models/Collection";
-import CollectionSharingSettings from "../CollectionSharingSettings";
-import OrderableContent from "../OrderableContent";
+import UserCollection from "../UserCollection";
 
 interface SharedPageState {
 	collections: Collection[],
-	settingsOpenCollectionIds: string[],
-	// Modal state
 	newCollectionDialogOpen: boolean,
-	addAlbumToCollectionDialogOpen: boolean,
-	confirmDeleteCollectionOpen: boolean,
-	confirmRemoveAlbumFromCollectionOpen: boolean,
-	// Temp variables during confirm modals
-	activeCollectionId: string | null,
-	activeAlbumId: string | null,
 }
 
 class SharedPage extends PageBaseComponent<SharedPageState> {
@@ -29,16 +17,11 @@ class SharedPage extends PageBaseComponent<SharedPageState> {
 		super(props);
 
 		this.onAlbumOrderChanged = this.onAlbumOrderChanged.bind(this);
+		this.refreshCollections = this.refreshCollections.bind(this);
 
 		this.state = {
 			collections: [],
-			settingsOpenCollectionIds: [],
-			newCollectionDialogOpen: false,
-			addAlbumToCollectionDialogOpen: false,
-			confirmDeleteCollectionOpen: false,
-			confirmRemoveAlbumFromCollectionOpen: false,
-			activeCollectionId: null,
-			activeAlbumId: null,
+			newCollectionDialogOpen: false
 		}
 	}
 
@@ -60,53 +43,9 @@ class SharedPage extends PageBaseComponent<SharedPageState> {
 	}
 
 	refreshCollections() {
-		const fnSetCollections = (collections: Collection[]) => this.setState({ collections });
-
 		PhotoService.getCollections()
-			.then(fnSetCollections)
+			.then(collections => this.setState({ collections }))
 			.catch(console.error);
-	}
-
-	openAlbum(albumId: string) {
-		this.context.history.push("/album/" + albumId);
-	}
-
-	deleteCollection(collectionId: string) {
-		PhotoService.deleteCollection(collectionId)
-			.then(() => this.refreshCollections())
-			.catch(console.error)
-			.finally(() => this.setState({confirmDeleteCollectionOpen: false}));
-	}
-
-	toggleSettings(collectionId: string) {
-		let collectionIds = this.state.settingsOpenCollectionIds;
-		const collectionIdIndex = collectionIds.indexOf(collectionId);
-		const collectionIdOpened = collectionIdIndex !== -1;
-
-		if (collectionIdOpened) {
-			collectionIds.splice(collectionIdIndex, 1);
-		}
-		else {
-			collectionIds.push(collectionId);
-		}
-
-		this.setState({
-			settingsOpenCollectionIds: collectionIds
-		});
-	}
-
-	addAlbumToCollection(collectionId: string, albumId: string) {
-		PhotoService.addAlbumToCollection(collectionId, albumId)
-			.then(() => this.refreshCollections())
-			.catch(console.error)
-			.finally(() => this.setState({addAlbumToCollectionDialogOpen: false}));
-	}
-
-	removeAlbumFromCollection(collectionId: string, albumId: string) {
-		PhotoService.removeAlbumFromCollection(collectionId, albumId)
-			.then(() => this.refreshCollections())
-			.catch(console.error)
-			.finally(() => this.setState({confirmRemoveAlbumFromCollectionOpen: false}));
 	}
 
 	onCreateCollectionClick() {
@@ -122,124 +61,26 @@ class SharedPage extends PageBaseComponent<SharedPageState> {
 			.finally(() => this.setState({newCollectionDialogOpen: false}));
 	}
 
-	onClickDeleteCollection(collectionId: string) {
-		this.setState({
-			confirmDeleteCollectionOpen: true,
-			activeCollectionId: collectionId
-		});
-	}
-
-	onAddAlbumToCollectionClick(collectionId: string) {
-		this.setState({
-			addAlbumToCollectionDialogOpen: true,
-			activeCollectionId: collectionId
-		});
-	}
-
-	onRemoveAlbumFromCollectionClick(collectionId: string, albumId: string) {
-		this.setState({
-			confirmRemoveAlbumFromCollectionOpen: true,
-			activeCollectionId: collectionId,
-			activeAlbumId: albumId
-		});
-	}
-
 	onAlbumOrderChanged(movedItem: unknown, newPosition: number): void {
 		console.log("onAlbumOrderChanged", movedItem, newPosition);
 	}
 
 	render() {
-		const activeCollection = this.state.collections.find(col => col.id === this.state.activeCollectionId);
-		const activeAlbum = activeCollection && activeCollection.albums.find(alb => alb.id === this.state.activeAlbumId);
-
 		return (
 			<ContentContainer paddingTop={true} className="collections">
 				{this.state.collections.map(collection => {
-					const settingsOpened = this.state.settingsOpenCollectionIds.indexOf(collection.id) !== -1;
-
-					// Collection container
-					return <div key={collection.id} className="collection">
-						<div className="head">
-							{/* Collection title and some actions/buttons */}
-							<h2 className="title">{collection.title}</h2>
-							<button className="iconOnly" onClick={() => this.toggleSettings(collection.id)} title="Collection sharing options">
-								{settingsOpened && <IconChevronUp/>}
-								{!settingsOpened && <IconChevronDown/>}
-							</button>
-							<button className="iconOnly" onClick={() => this.onClickDeleteCollection(collection.id)} title="Delete collection">
-								<IconDelete/>
-							</button>
-						</div>
-
-						<div className="body">
-							<div className={"settings" + (settingsOpened ? " open" : "")}>
-								{settingsOpened && <CollectionSharingSettings collection={collection} onOptionsChanged={() => this.refreshCollections()}/>}
-							</div>
-							{settingsOpened && <hr/>}
-
-							{/* Albums inside this collection */}
-							<div className="">
-								<OrderableContent
-									className="collection-albums"
-									onOrderChanged={this.onAlbumOrderChanged}>
-									{collection.albums.map(album => {
-										let albumThumbUrl = album.thumbPhotoId
-											? "url('" + PhotoService.baseUrl() + "/photo/" + album.thumbPhotoId + "/thumb')"
-											: "";
-
-										return (<div key={album.id}
-											className="album"
-											style={{ backgroundImage: albumThumbUrl }}
-											onClick={() => this.openAlbum(album.id)}>
-											<div className="albumContent">
-												<span className="title">{album.title}</span>
-												<button className="iconOnly" onClick={(event) => {
-													event.stopPropagation();
-													this.onRemoveAlbumFromCollectionClick(collection.id, album.id);
-												}} title="Remove album from collection">
-													<IconClose/>
-												</button>
-											</div>
-										</div>);
-									})}
-								</OrderableContent>
-								<button className="iconOnly" onClick={() => this.onAddAlbumToCollectionClick(collection.id)} title="Add album to collection">
-									<IconCreate/> TODO
-								</button>
-							</div>
-						</div>
-					</div>;
+					return <UserCollection
+						key={collection.id}
+						collection={collection}
+						onCollectionUpdated={this.refreshCollections}
+						onCollectionDeleted={this.refreshCollections}
+						/>
 				})}
 
-
-				{this.state.addAlbumToCollectionDialogOpen && <ModalAddAlbumToCollection
-					isOpen={true}
-					onRequestClose={() => this.setState({addAlbumToCollectionDialogOpen: false})}
-					onAlbumSelected={(album) => this.addAlbumToCollection(this.state.activeCollectionId!, album.id)}
-					/>}
 				{this.state.newCollectionDialogOpen && <ModalCreateCollection
 					isOpen={true}
 					onRequestClose={() => this.setState({newCollectionDialogOpen: false})}
 					onOkButtonClick={(title) => this.createCollection(title)}
-					/>}
-				{this.state.confirmDeleteCollectionOpen && <ModalConfirmation
-					title="Delete collection"
-					isOpen={true}
-					onRequestClose={() => this.setState({confirmDeleteCollectionOpen: false})}
-					onOkButtonClick={() => this.deleteCollection(this.state.activeCollectionId!)}
-					okButtonText="Delete"
-					confirmationText={"Collection '" + activeCollection!.title + "' will be deleted."}
-					/>}
-				{this.state.confirmRemoveAlbumFromCollectionOpen && <ModalConfirmation
-					title="Remove album from collection"
-					isOpen={true}
-					onRequestClose={() => this.setState({confirmRemoveAlbumFromCollectionOpen: false})}
-					onOkButtonClick={() => this.removeAlbumFromCollection(this.state.activeCollectionId!, this.state.activeAlbumId!)}
-					okButtonText="Remove"
-					confirmationText={"Album '"
-						+ activeCollection!.title
-						+ "' will be remove from collection '"
-						+ activeAlbum!.title + "'."}
 					/>}
 			</ContentContainer>
 		);
