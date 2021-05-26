870bc1ae6ea4efb80b02be0bcba792f3c6582173
Eric Liefaart <eric@e10t.nl>
2021-05-26 22:44:01 +0200

    Add proof-of-concept encrypt/decrypt functions to wasm

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -3,13 +3,26 @@ import * as ReactDOM from "react-dom";
 import AppContainer from "./components/AppContainer";
 import "./skin/app.scss";
 
-import init, {greet, hello} from "hello-wasm";
+import init, {get_struct, get_string, get_array_u32, get_array_u8, aes256_encrypt, aes256_decrypt} from "hello-wasm";
 
 // Render a page in container
 const rootElement = document.getElementById("appRoot");
 ReactDOM.render(<AppContainer/>, rootElement);
 
 init("dist/hello_wasm.wasm").then(() => {
-	console.warn(hello("eric"));
-	greet("WebAssembly");
+	const struct = get_struct();
+	console.log(struct);
+	console.log(struct.id);
+	console.log(struct.something);
+
+	console.log(get_array_u32());
+	console.log(get_array_u8());
+	console.log(get_array_u8().buffer);
+
+	console.log(get_string("abc..def"));
+
+	const bytes = get_array_u8();
+	const encrypted = aes256_encrypt(bytes);
+	const decrypted = aes256_decrypt(encrypted);
+	console.log(bytes, encrypted, decrypted);
 });
