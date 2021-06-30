57ba2db4fcbe8d2655108f7198743a24ae5c83b5
Eric Liefaart <eric@e10t.nl>
2021-06-30 22:17:02 +0200

    Parsing image bytes, generating thumbs etc now function within wasm

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -3,26 +3,13 @@ import * as ReactDOM from "react-dom";
 import AppContainer from "./components/AppContainer";
 import "./skin/app.scss";
 
-import init, {get_struct, get_string, get_array_u32, get_array_u8, aes256_encrypt, aes256_decrypt} from "hello-wasm";
+import init from "wasm";
+import uploadHelper from "./helpers/UploadHelperNew";
 
 // Render a page in container
 const rootElement = document.getElementById("appRoot");
 ReactDOM.render(<AppContainer/>, rootElement);
 
-init("dist/hello_wasm.wasm").then(() => {
-	const struct = get_struct();
-	console.log(struct);
-	console.log(struct.id);
-	console.log(struct.something);
-
-	console.log(get_array_u32());
-	console.log(get_array_u8());
-	console.log(get_array_u8().buffer);
-
-	console.log(get_string("abc..def"));
-
-	const bytes = get_array_u8();
-	const encrypted = aes256_encrypt(bytes);
-	const decrypted = aes256_decrypt(encrypted);
-	console.log(bytes, encrypted, decrypted);
+init("dist/wasm.wasm").then(() => {
+	uploadHelper.init();
 });
