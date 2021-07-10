1d0ac3a8dd644df3a2d0d7a3f0f7417f8ea22d57
Eric Liefaart <eric@e10t.nl>
2021-07-10 23:12:46 +0200

    Improvements to wasm interface

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,8 +1,14 @@
 use std::error::Error;
 use exif::Exif;
-use image::{GenericImageView, ImageFormat};
-use wasm_bindgen::{prelude::*};
+use js_sys::Array;
+use wasm_bindgen::prelude::*;
 use uuid::Uuid;
+use wasm_bindgen_futures::future_to_promise;
+
+/*
+ * Info on async functions within struct implementations:
+ * https://github.com/rustwasm/wasm-bindgen/issues/1858
+ */
 
 mod error;
 mod aes256;
@@ -24,9 +30,6 @@ extern "C" {
 	#[wasm_bindgen(js_namespace = console)]
 	fn error(s: &str);
 }
-
-//pub fn example(input: &str) -> String {}
-//pub async fn example_async() -> String {}
 
 #[wasm_bindgen]
 pub fn generate_aes256_key() -> js_sys::Uint8Array {
@@ -62,80 +65,8 @@ pub fn aes256_decrypt(key: &[u8], nonce: String, buffer: &[u8]) -> js_sys::Uint8
 #[wasm_bindgen]
 pub struct ImageUploadInfo {
 	image: images::Image,
-	//pub bytes: js_sys::Uint8Array,
-	//pub bytes: Vec<u8>,
-	exif: exif::Exif
-	//exif: Exif
+	exif: Exif
 }
-#[wasm_bindgen]
-pub struct UpholiExif {
-	exif: exif::Exif
-}
-
-#[wasm_bindgen]
-impl UpholiExif{
-	pub fn get_manufactorer(&mut self) -> js_sys::JsString {
-        String::from("").into()
-    }
-}
-
-// impl wasm_bindgen::convert::IntoWasmAbi for Option<String> {
-//     type Abi = JsValue;
-
-//     fn into_abi(self) -> Self::Abi {
-//         JsValue::from_serde(&self).unwrap().into_abi()
-//     }
-// }
-// #[wasm_bindgen]
-// pub struct UpholiExif {
-// 	pub manufactorer: js_sys::JsString,
-// 	pub model: Option<js_sys::JsString>,
-// 	pub aperture: Option<js_sys::JsString>,
-// 	pub exposure_time: Option<js_sys::JsString>,
-// 	pub iso: Option<i32>,
-// 	pub focal_length: Option<i32>,
-// 	pub focal_length_35mm_equiv: Option<i32>,
-// 	pub orientation: Option<i32>,
-// 	pub date_taken: Option<js_sys::Date>,
-// 	pub gps_latitude: Option<f32>,
-// 	pub gps_longitude: Option<f32>
-// }
-
-// #[wasm_bindgen]
-// pub struct Exif {
-// 	manufactorer: Option<String>,
-// 	pub iso: Option<i32>,
-// 	#[wasm_bindgen(js_name = focalLength)]
-// 	pub focal_length: Option<i32>,
-// 	#[wasm_bindgen(js_name = focalLength35mmEquiv)]
-// 	pub focal_length_35mm_equiv: Option<i32>,
-// }
-
-// impl Copy for Exif { }
-// impl Clone for Exif {
-//     fn clone(&self) -> Exif {
-//         Exif {
-// 			manufactorer: self.manufactorer.clone(),
-// 			iso: self.iso,
-// 			focal_length: self.focal_length,
-// 			focal_length_35mm_equiv: self.focal_length_35mm_equiv
-// 		}
-//     }
-// }
-
-#[wasm_bindgen]
-pub async fn test_reqwest() -> String {
-	match reqwest::get("https://httpbin.org/ip").await {
-		Ok(response) => {
-			match response.json::<std::collections::HashMap<String, String>>().await {
-				Ok(json) => format!("{:#?}", json),
-				Err(_) => String::from("")
-			}
-		},
-		Err(_) => String::from("")
-	}
-}
-
 
 #[wasm_bindgen]
 impl ImageUploadInfo {
@@ -146,7 +77,6 @@ impl ImageUploadInfo {
 			Ok(exif) => {
 				let exif_orientation = exif.orientation.unwrap_or(1);
 
-				//let slice = &bytes[0..];
 				let image = images::Image::from_buffer(bytes, exif_orientation as u8).unwrap();
 
 				ImageUploadInfo {
@@ -158,50 +88,40 @@ impl ImageUploadInfo {
 		}
     }
 
-
-
 	#[wasm_bindgen(getter)]
     pub fn bytes(&self) -> Vec<u8> {
-        self.image.bytes_original[0..20].to_vec()
+        self.image.bytes_original[..].to_vec()
     }
 
-	#[wasm_bindgen(getter, js_name = exifFocalLength)]
-    pub fn exif_focal_length(&self) -> Option<i32> {
-        self.exif.focal_length
+	#[wasm_bindgen(getter, js_name = bytesPreview)]
+    pub fn bytes_preview(&self) -> Vec<u8> {
+        self.image.bytes_preview[..].to_vec()
     }
 
-	#[wasm_bindgen(getter, js_name = exifManufactorer)]
-    pub fn exif_manufacturer(&self) -> Option<String> {
-        self.exif.manufactorer.to_owned()
+	#[wasm_bindgen(getter, js_name = bytesThumbnail)]
+    pub fn bytes_thumbnail(&self) -> Vec<u8> {
+        self.image.bytes_thumbnail[..].to_vec()
     }
 
-	pub fn get_bytes(&mut self) -> js_sys::Uint8Array {
-        js_sys::Uint8Array::from(&self.image.bytes_original[..])
-    }
-
-	pub fn get_preview_bytes(&mut self) -> js_sys::Uint8Array {
-        js_sys::Uint8Array::from(&self.image.bytes_preview[..])
-    }
-
-	pub fn get_thumbnail_bytes(&mut self) -> js_sys::Uint8Array {
-        js_sys::Uint8Array::from(&self.image.bytes_thumbnail[..])
+	#[wasm_bindgen(getter, js_name = exif)]
+    pub fn exif(&self) -> JsValue {
+        match JsValue::from_serde(&self.exif) {
+			Ok(exif) => {
+				exif
+			},
+			Err(error) => JsValue::from(format!("Error serializing: {}", error))
+		}
     }
 }
 
 
 
-
-
-
-
-
-
-// #[wasm_bindgen]
-// pub struct UpholiPhotoMinimal {
-// 	id: String,
-// 	pub width: u32,
-// 	pub height: u32
-// }
+#[derive(serde::Deserialize, serde::Serialize)]
+struct UpholiPhotoMinimal {
+	id: String,
+	width: u32,
+	height: u32
+}
 
 // #[wasm_bindgen]
 // pub struct UpholiPhoto {
@@ -218,42 +138,74 @@ impl ImageUploadInfo {
 // #[wasm_bindgen]
 // pub struct UpholiExif { }
 
-// #[wasm_bindgen]
-// pub struct UpholiClient {
-// 	base_url: String
+// #[wasm_bindgen(js_name = getPhotos)]
+// pub async fn get_photos() -> Array {
+// 	let vec: Vec<u32> = Vec::new();
+// 	vec.into()
 // }
 
-// #[wasm_bindgen]
-// impl UpholiClient {
-// 	#[wasm_bindgen(constructor)]
-//     pub fn new(base_url: String) -> UpholiClient {
-// 		UpholiClient {
-// 			base_url
-// 		}
-// 	}
+#[wasm_bindgen]
+pub struct UpholiClient {
+	base_url: String
+}
 
-// 	pub fn get_photos(&mut self) -> Vec<UpholiPhotoMinimal> {
-// 		vec!{
-// 			UpholiPhotoMinimal {
-// 				id: "1".to_string(),
-// 				height: 20,
-// 				width: 10
-// 			}
-// 		}
-// 	}
+/// Client for Upholi server.
+#[wasm_bindgen]
+impl UpholiClient {
+	#[wasm_bindgen(constructor)]
+	pub fn new(base_url: String) -> UpholiClient {
+		UpholiClient {
+			base_url
+		}
+	}
 
-// 	pub fn get_array(&self) -> Vec<JsValue> {
-// 		let photos: Vec<UpholiPhotoMinimal> = vec!{};
-// 		photos.iter().map(JsValue::from).collect()
-// 	}
+	#[wasm_bindgen(js_name = uploadPhoto)]
+	pub fn upload_photo(&self, image: ImageUploadInfo) -> js_sys::Promise {
+		future_to_promise(async move {
+			Ok(JsValue::NULL)
+		})
+	}
 
-// 	// pub async fn get_photo(&mut self, id: String) {}
-// 	// pub async fn get_photo_bytes_original(&mut self, id: String) {}
-// 	// pub async fn get_photo_bytes_preview(&mut self, id: String) {}
-// 	// pub async fn get_photo_bytes_thumbnail(&mut self, id: String) {}
+	#[wasm_bindgen(js_name = getPhotos)]
+	pub fn get_photos(&self) -> js_sys::Promise {
+		let url = format!("{}/api/photos", &self.base_url).to_owned();
 
-// 	// pub async fn get_albums(&mut self) {}
-// 	// pub async fn get_album(&mut self, id: String) {}
-// 	// pub async fn insert_album(&mut self) {}
-// 	// pub async fn update_album(&mut self) {}
-// }
+		future_to_promise(async move {
+			match reqwest::get(url).await {
+				Ok(response) => {
+					match response.json::<Vec<UpholiPhotoMinimal>>().await {
+						Ok(photos) => {
+							//let js_array = Array::new_with_length(photos.len() as u32);
+							let mut js_array_photos: Vec<JsValue> = Vec::new();
+
+							for photo in photos {
+								let photo = JsValue::from_serde(&photo).unwrap_throw();
+								js_array_photos.push(photo);
+							}
+
+							let js_array_photos = JsValue::from(js_array_photos.iter().collect::<Array>());
+							Ok(js_array_photos)
+						},
+						Err(error) => Err(String::from(format!("{}", error)).into())
+					}
+				},
+				Err(error) => Err(String::from(format!("{}", error)).into())
+			}
+		})
+	}
+
+	// pub fn get_array(&self) -> Vec<JsValue> {
+	// 	let photos: Vec<UpholiPhotoMinimal> = vec!{};
+	// 	photos.iter().map(JsValue::from).collect()
+	// }
+
+	// pub async fn get_photo(&mut self, id: String) {}
+	// pub async fn get_photo_bytes_original(&mut self, id: String) {}
+	// pub async fn get_photo_bytes_preview(&mut self, id: String) {}
+	// pub async fn get_photo_bytes_thumbnail(&mut self, id: String) {}
+
+	// pub async fn get_albums(&mut self) {}
+	// pub async fn get_album(&mut self, id: String) {}
+	// pub async fn insert_album(&mut self) {}
+	// pub async fn update_album(&mut self) {}
+}
