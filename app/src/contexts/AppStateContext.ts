ee24a382dfb7aeef3ea17a9032a8960b4394c8db
Eric Liefaart <eric@e10t.nl>
2021-11-15 22:36:05 +0100

    Remove unused fields from AppState

--- app/src/contexts/AppStateContext.ts
+++ app/src/contexts/AppStateContext.ts
@@ -1,17 +1,12 @@
 import * as React from "react";
 import { History, createBrowserHistory } from "history";
-import { HeaderSettings } from "../hooks/useHeader";
 
 export interface AppState {
-	history: History,
-	header: HeaderSettings
+	history: History
 }
 
 const appStateContext: React.Context<AppState> = React.createContext<AppState>({
 	history: createBrowserHistory(),
-	header: {
-		visible: false
-	}
 });
 
 export default appStateContext;
