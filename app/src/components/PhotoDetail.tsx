61b9471621b528f3acb1254a3f277d96ca6eedfa
Eric Liefaart <eric@e10t.nl>
2022-01-04 23:11:31 +0100

    Revert "Improve zoom performance PhotoDetail component by throttling how often touchmove event is handled"
    This reverts commit e81a22110fe7b508b82b2f780805ec5f3fadab19.

--- app/src/components/PhotoDetail.tsx
+++ app/src/components/PhotoDetail.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import ExifData from "./misc/ExifData";
 import Exif from "../models/Exif";
-import _ = require("underscore");
 
 interface PhotoDetailProps {
 	src: string;
@@ -58,7 +57,6 @@ class PhotoDetail extends React.Component<PhotoDetailProps, PhotoDetailState> {
 			fnHandlePanning(event);
 			fnHandlePinching(event);
 		};
-		const fnOnTouchMoveThrottled = _.throttle(fnOnTouchMove, 16);	// Target: 60fps
 
 		// Handle panning, moving image along x and y axis
 		const fnHandlePanning = (event: MouseEvent | TouchEvent) => {
@@ -76,7 +74,6 @@ class PhotoDetail extends React.Component<PhotoDetailProps, PhotoDetailState> {
 				panLastY = currentY;
 			}
 		};
-		const fnHandlePanningThrottled = _.throttle(fnHandlePanning, 11);	// Target: 90fps
 
 		// Handle touch pinching: zooming
 		let fingerDistanceLast: number;
@@ -118,8 +115,8 @@ class PhotoDetail extends React.Component<PhotoDetailProps, PhotoDetailState> {
 		containerElement.onmouseleave = fnStopPanning;
 		containerElement.ontouchend = fnStopPanning;
 
-		containerElement.onmousemove = fnHandlePanningThrottled;
-		containerElement.ontouchmove = fnOnTouchMoveThrottled;
+		containerElement.onmousemove = fnHandlePanning;
+		containerElement.ontouchmove = fnOnTouchMove;
 
 		containerElement.ondblclick = fnOnDoubleClick;
 	}
