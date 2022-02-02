17a891124737aceaf68ba41a02c0b122f4c96cd2
Eric Liefaart <eric@e10t.nl>
2022-02-02 22:15:21 +0100

    Refactor user handlers

--- server/src/web/handlers/users.rs
+++ server/src/web/handlers/users.rs
@@ -6,62 +6,54 @@ use crate::{
 	error::{LoginError, RegisterError},
 	web::{
 		cookies::create_session_cookie,
-		http::{create_bad_request_response, create_internal_server_error_response, create_ok_response, get_session_or_create_new},
+		http::{create_ok_response, get_session_or_create_new},
 	},
 };
-use actix_web::{web, HttpResponse, Responder};
+use actix_web::error::{ErrorBadRequest, ErrorInternalServerError};
+use actix_web::{web, HttpResponse, Result};
 use upholi_lib::http::request::{Login, Register};
 
-pub async fn route_register_user(info: web::Json<Register>) -> impl Responder {
+pub async fn route_register_user(info: web::Json<Register>) -> Result<HttpResponse> {
 	let info = info.into_inner();
 
 	if info.username.is_empty() {
-		create_bad_request_response(Box::from(RegisterError::UsernameEmpty))
+		Err(ErrorInternalServerError(RegisterError::UsernameEmpty))
 	} else if info.password.len() < crate::SETTINGS.users.password_min_length {
-		create_bad_request_response(Box::from(RegisterError::PasswordTooShort))
+		Err(ErrorBadRequest(RegisterError::PasswordTooShort))
 	} else {
-		match User::create(info.username, info.password, info.key).await {
-			Ok(_) => create_ok_response(),
-			Err(error) => create_internal_server_error_response(Some(error)),
-		}
+		User::create(info.username, info.password, info.key)
+			.await
+			.map_err(|error| ErrorInternalServerError(error))?;
+		Ok(create_ok_response())
 	}
 }
 
-pub async fn route_login_user(session: Option<Session>, info: web::Json<Login>) -> impl Responder {
+pub async fn route_login_user(session: Option<Session>, info: web::Json<Login>) -> Result<HttpResponse> {
 	let info = info.into_inner();
 
-	match User::get_by_username(&info.username).await {
-		Ok(user) => match user {
-			Some(user) => {
-				if user.password_valid(&info.password) {
-					match get_session_or_create_new(session).await {
-						Ok(mut session) => {
-							session.set_user(&user.id);
-							match session.update().await {
-								Ok(_) => {
-									let mut response = HttpResponse::Ok().json(user);
-									let cookie = create_session_cookie(&session);
+	let user = User::get_by_username(&info.username)
+		.await
+		.map_err(|error| ErrorInternalServerError(error))?
+		.ok_or_else(|| ErrorBadRequest(LoginError::InvalidCredentials))?;
 
-									match response.add_cookie(&cookie) {
-										Ok(_) => response,
-										Err(error) => create_internal_server_error_response(Some(Box::new(error))),
-									}
-								}
-								Err(error) => create_internal_server_error_response(Some(error)),
-							}
-						}
-						Err(error) => create_internal_server_error_response(Some(error)),
-					}
-				} else {
-					create_bad_request_response(Box::from(LoginError::InvalidCredentials))
-				}
-			}
-			None => create_bad_request_response(Box::from(LoginError::InvalidCredentials)),
-		},
-		Err(error) => create_internal_server_error_response(Some(error)),
+	if user.password_valid(&info.password) {
+		let mut session = get_session_or_create_new(session)
+			.await
+			.map_err(|error| ErrorInternalServerError(error))?;
+
+		session.set_user(&user.id);
+		session.update().await.map_err(|error| ErrorInternalServerError(error))?;
+		let mut response = HttpResponse::Ok().json(user);
+		let cookie = create_session_cookie(&session);
+
+		response.add_cookie(&cookie).map_err(|error| ErrorInternalServerError(error))?;
+
+		Ok(response)
+	} else {
+		Err(ErrorBadRequest(LoginError::InvalidCredentials))
 	}
 }
 
-pub async fn route_user_info(user: User) -> impl Responder {
-	HttpResponse::Ok().json(user)
+pub async fn route_user_info(user: User) -> Result<HttpResponse> {
+	Ok(HttpResponse::Ok().json(user))
 }
