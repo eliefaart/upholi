373342a95ab827cd89af11eb8a0263843a24bda0
Eric Liefaart <eric@e10t.nl>
2022-04-05 22:52:45 +0200

    Sort albums on albums page

--- app/src/components/pages/AlbumsPage.tsx
+++ app/src/components/pages/AlbumsPage.tsx
@@ -12,7 +12,7 @@ import DefaultHeaderContent from "../headers/DefaultHeaderContent";
 
 const AlbumsPage: FC<PageProps> = (props: PageProps) => {
 	const [newAlbumDialogOpen, setNewAlbumDialogOpen] = React.useState(false);
-	const [albums] = useAlbums();
+	const albums = useAlbums();
 	const context = React.useContext(appStateContext);
 
 	useTitle("Albums");
@@ -30,10 +30,49 @@ const AlbumsPage: FC<PageProps> = (props: PageProps) => {
 		});
 	}, []);
 
+	/**
+	 * Sort albums.
+	 * Basically albums are sorted by title alphabetically.
+	 * However, albums with a year (eg 2022) in the title are moved to the front and are sorted by year descending.
+	 * It's a bit magical, but that is fine for now. :D
+	 */
+	const sortedAlbums = albums.sort((one, two) => {
+		const regex = new RegExp(/.?[0-9]{4}.?/);
+
+		// Create a string that album can be sorted by, and returns if album title contains a year.
+		const getSortingTitle = (title: string): [string, boolean] => {
+			const matches = regex.exec(title);
+			const containsYear = matches !== null && matches.length > 0;
+
+			if (containsYear) {
+				const year = matches[0];
+				title = year + title;
+			}
+
+			return [title, containsYear];
+		};
+
+		const [oneTitle, oneContainsYear] = getSortingTitle(one.title);
+		const [twoTitle, twoContainsYear] = getSortingTitle(two.title);
+
+		if (oneContainsYear && twoContainsYear) {
+			// Sort as descending
+			return oneTitle > twoTitle ? -1 : 1;
+		}
+		else if (oneContainsYear !== twoContainsYear) {
+			// Sort so that the title with the year is before the one without year
+			return oneContainsYear ? -1 : 1;
+		}
+		else {
+			// Sort as ascending
+			return oneTitle > twoTitle ? 1 : -1;
+		}
+	});
+
 	return (
 		<Content paddingTop={false}>
 			<TagGroupedAlbums
-				albums={albums}
+				albums={sortedAlbums}
 				onAlbumClick={album => context.history.push("/album/" + album.id)} />
 
 			<ModalCreateAlbum
--- app/src/hooks/useAlbums.ts
+++ app/src/hooks/useAlbums.ts
@@ -2,6 +2,8 @@ import { AlbumPlain } from "../models/Album";
 import upholiService from "../services/UpholiService";
 import { useApiResource } from "./useApiResource";
 
-export default function useAlbums(): [AlbumPlain[], () => void] {
-	return useApiResource<AlbumPlain[]>(() => upholiService.getAlbums(), []);
+export default function useAlbums(): AlbumPlain[] {
+	const [albums] = useApiResource<AlbumPlain[]>(() => upholiService.getAlbums(), []);
+
+	return albums;
 }
