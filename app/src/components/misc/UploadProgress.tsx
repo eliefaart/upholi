52b5bd21db4217bcef54a31e33e5f276c9be4a82
Eric Liefaart <eric@e10t.nl>
2023-02-05 22:05:15 +0100

    Improvements to upload progress modal

--- app/src/components/misc/UploadProgress.tsx
+++ app/src/components/misc/UploadProgress.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { FC } from "react";
-import { uploadFinishedStatusses } from "../../models/File";
+import { FileUploadStatus, uploadFinishedStatusses } from "../../models/File";
 import { IconClose } from "./Icons";
 import uploadHelper from "../../helpers/UploadHelper";
 import useUploadProgress from "../../hooks/useUploadProgress";
@@ -23,12 +23,18 @@ const UploadProgress: FC = () => {
 		return null;
 	}
 	else {
+		const nTotal = uploadProgress.length;
+		const nDone = uploadProgress.filter(p => p.status === FileUploadStatus.Done).length;
+
 		return <div className="upload-progress">
 			<div className="header">
-				{queueFinished && <Button
-					onClick={() => uploadHelper.clearQueue()}
-					label=""
-					icon={<IconClose />} />}
+				<span className="title">Uploaded {nDone}/{nTotal}</span>
+				<div className="actions">
+					{queueFinished && <Button
+						onClick={() => uploadHelper.clearQueue()}
+						label=""
+						icon={<IconClose />} />}
+				</div>
 			</div>
 			<div className="files">
 				{uploadProgress.map(file => (
