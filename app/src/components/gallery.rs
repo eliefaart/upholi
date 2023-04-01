39a1520801a09cf8c1a9f21cf248bc58095f33df
Eric Liefaart <eric@e10t.nl>
2023-04-01 16:28:42 +0200

    Fix for last row in gallery

--- app/src/components/gallery.rs
+++ app/src/components/gallery.rs
@@ -4,7 +4,12 @@ use yew::prelude::*;
 use yew_hooks::use_interval;
 use yew_router::prelude::*;
 
+// Desired minimum height of a gallery photo.
 const MIN_HEIGHT: f32 = 175.;
+// Desired minimum height of a gallery photo.
+const MAX_HEIGHT: f32 = 350.;
+// Pixels between each photo, as per CSS.
+const GAP_SIZE: f32 = 5.;
 
 #[derive(Properties, PartialEq)]
 pub struct GalleryProps {
@@ -47,7 +52,6 @@ pub fn gallery(props: &GalleryProps) -> Html {
         );
     }
 
-    // TODO: Don't bother with this on first render.
     if available_width.is_none() {
         let gallery_element = node_ref.cast::<Element>();
         if let Some(gallery_element) = gallery_element {
@@ -77,7 +81,7 @@ pub fn gallery(props: &GalleryProps) -> Html {
     let photos = use_memo(
         |(available_width, photos, selected_photos)| {
             if let Some(available_width) = available_width {
-                let photos = compute_sizes(&photos, *available_width, MIN_HEIGHT);
+                let photos = compute_sizes(&photos, *available_width, MIN_HEIGHT, MAX_HEIGHT, GAP_SIZE);
                 photos
                     .into_iter()
                     .map(|ResizedPhoto { photo, width, height }| {
@@ -132,7 +136,13 @@ struct ResizedPhoto<'a> {
     height: f32,
 }
 
-fn compute_sizes<'a>(photos: &'a Vec<AlbumPhoto>, available_width: f32, min_height: f32) -> Vec<ResizedPhoto> {
+fn compute_sizes<'a>(
+    photos: &'a Vec<AlbumPhoto>,
+    available_width: f32,
+    min_height: f32,
+    max_height: f32,
+    gap_size: f32,
+) -> Vec<ResizedPhoto> {
     let mut rows: Vec<Vec<ResizedPhoto>> = photos
         .into_iter()
         // Normalize all photos to minimum height
@@ -147,7 +157,7 @@ fn compute_sizes<'a>(photos: &'a Vec<AlbumPhoto>, available_width: f32, min_heig
         .fold(vec![vec![]], |mut accumulator, photo| {
             let current_row = accumulator.last_mut().expect("Make sure the accumulator starts with one row");
 
-            let row_width: f32 = current_row.iter().map(|p| p.width).sum();
+            let row_width: f32 = current_row.iter().map(|p| p.width + gap_size).sum();
 
             if row_width + photo.width > available_width {
                 // new row
@@ -162,12 +172,25 @@ fn compute_sizes<'a>(photos: &'a Vec<AlbumPhoto>, available_width: f32, min_heig
 
     // Increase each photo's size to fully fill the row's width
     for row in &mut rows {
-        let row_width: f32 = row.iter().map(|p| p.width).sum();
+        let row_width: f32 = row.iter().map(|p| p.width + gap_size).sum::<f32>() - gap_size;
         let bloat_ratio = available_width / row_width;
 
         for mut photo in row {
             photo.width = photo.width * bloat_ratio;
             photo.height = photo.height * bloat_ratio;
+        }
+    }
+
+    // Last row may not contain enough photos to properly fill the row.
+    // But since we let it fill its max width anyway, the photos may be stretched out a lot.
+    // Resize them to meet the 'max_height' constraint.
+    let last_row = rows.last_mut();
+    if let Some(last_row) = last_row {
+        for photo in last_row {
+            if photo.height > max_height {
+                photo.width = photo.width / (photo.height / max_height);
+                photo.height = max_height;
+            }
         }
     }
 
