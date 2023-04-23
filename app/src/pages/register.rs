167d41e6abb94733d0c6de40e9cbcbb7a12c23bd
Eric Liefaart <eric@e10t.nl>
2023-04-23 19:58:35 +0200

    Fix redirect after register

--- app/src/pages/register.rs
+++ app/src/pages/register.rs
@@ -1,11 +1,13 @@
-use crate::{components::Form, WASM_CLIENT};
+use crate::{components::Form, Route, WASM_CLIENT};
 use web_sys::HtmlInputElement;
 use yew::prelude::*;
+use yew_router::prelude::use_navigator;
 
 #[function_component(RegisterPage)]
 pub fn register() -> Html {
     let username_ref = use_node_ref();
     let password_ref = use_node_ref();
+    let navigator = use_navigator().unwrap();
 
     let on_click_create = {
         let username_ref = username_ref.clone();
@@ -18,10 +20,12 @@ pub fn register() -> Html {
             ) {
                 let username = username_input.value();
                 let password = password_input.value();
+                let navigator = navigator.clone();
 
                 if !password.is_empty() && !username.is_empty() {
                     wasm_bindgen_futures::spawn_local(async move {
                         WASM_CLIENT.register(&username, &password).await.unwrap();
+                        navigator.push(&Route::Home)
                     });
                 }
             }
