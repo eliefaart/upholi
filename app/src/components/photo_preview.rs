4f335751f1668660979d8bd335efc36424f84274
Eric Liefaart <eric@e10t.nl>
2023-06-26 23:17:38 +0200

    Attempt implement pinch zooming

--- app/src/components/photo_preview.rs
+++ app/src/components/photo_preview.rs
@@ -1,8 +1,11 @@
-use crate::hooks::{use_photo, use_photo_src::use_photo_src};
+use crate::hooks::use_photo_src::use_photo_src;
 use std::ops::{AddAssign, Sub};
 use upholi_lib::PhotoVariant;
 use web_sys::HtmlElement;
 use yew::prelude::*;
+
+const ZOOM_FACTOR_MIN: f64 = 1.0;
+const ZOOM_FACTOR_MAX: f64 = 3.0;
 
 #[derive(Debug, PartialEq, Clone, Copy)]
 struct PhotoViewState {
@@ -11,6 +14,69 @@ struct PhotoViewState {
     is_panning: bool,
     previous: Option<XY>,
     finger_distance: Option<f64>,
+}
+
+// fn update_view_state(state: &UseStateHandle<PhotoViewState>, zoom: Option<f64>, offset: Option<XY>) {
+//     let mut new_state = **state;
+
+//     if let Some(mut zoom) = zoom {
+//         zoom = f64::clamp(zoom, ZOOM_FACTOR_MIN, ZOOM_FACTOR_MAX);
+//         new_state.zoom = zoom;
+//     }
+
+//     if let Some(offset) = offset {
+//         new_state.offset = offset;
+//     }
+
+//     state.set(new_state);
+// }
+
+impl PhotoViewState {
+    fn pan(&mut self, client_xy: (i32, i32), container_node: &NodeRef, photo_node: &NodeRef) {
+        let (client_x, client_y) = client_xy;
+
+        if self.is_panning {
+            let current = XY(client_x, client_y);
+            let mut offset = self.offset;
+
+            if let Some(prev) = self.previous {
+                let delta = current - prev;
+                offset += delta;
+
+                clamp_photo_offset(&mut offset, self.zoom, container_node, photo_node);
+            }
+
+            self.offset = offset;
+            self.previous = Some(current);
+        }
+    }
+
+    fn pinch(&mut self, finger_distance: f64, container_node: &NodeRef, photo_node: &NodeRef) {
+        if let Some(prev_distance) = self.finger_distance {
+            if let Some(photo_element) = photo_node.cast::<HtmlElement>() {
+                let photo_width = photo_element.client_width() as f64;
+                let delta = prev_distance - finger_distance;
+                let zoom_step = (delta / photo_width) * 100.;
+                let zoom_step = (self.zoom / 100.) * zoom_step;
+                let new_zoom = self.zoom - zoom_step;
+                self.set_zoom(new_zoom, container_node, photo_node);
+                // let new_zoom = f64::clamp(new_zoom, ZOOM_FACTOR_MIN, ZOOM_FACTOR_MAX);
+                // self.zoom = new_zoom;
+            }
+        }
+
+        self.finger_distance = Some(finger_distance);
+    }
+
+    fn set_zoom(&mut self, target_zoom: f64, container_node: &NodeRef, photo_node: &NodeRef) {
+        let target_zoom = f64::clamp(target_zoom, ZOOM_FACTOR_MIN, ZOOM_FACTOR_MAX);
+
+        let mut offset = self.offset;
+        clamp_photo_offset(&mut offset, target_zoom, &container_node, &photo_node);
+
+        self.zoom = target_zoom;
+        self.offset = offset;
+    }
 }
 
 impl Default for PhotoViewState {
@@ -107,6 +173,7 @@ pub fn photo_preview(props: &PhotoPreviewProps) -> Html {
                 let mut new_state = **view_state;
                 if !panning {
                     new_state.previous = None;
+                    new_state.finger_distance = None;
                 }
                 new_state.is_panning = panning;
                 view_state.set(new_state);
@@ -126,13 +193,14 @@ pub fn photo_preview(props: &PhotoPreviewProps) -> Html {
                 let zooming_in = event.delta_y() < 0.;
                 let zoom_step_percentage = if zooming_in { 15. } else { -15. };
                 let zoom = new_state.zoom + ((new_state.zoom / 100.) * zoom_step_percentage);
-                let zoom = f64::clamp(zoom, 1.0, 3.0);
+                new_state.set_zoom(zoom, &container_node, &photo_node);
+                // let zoom = f64::clamp(zoom, ZOOM_FACTOR_MIN, ZOOM_FACTOR_MAX);
 
-                let mut offset = view_state.offset;
-                clamp_photo_offset(&mut offset, zoom, &container_node, &photo_node);
+                // let mut offset = view_state.offset;
+                // clamp_photo_offset(&mut offset, zoom, &container_node, &photo_node);
 
-                new_state.zoom = zoom;
-                new_state.offset = offset;
+                // new_state.zoom = zoom;
+                // new_state.offset = offset;
                 view_state.set(new_state);
             },
             view_state.clone(),
@@ -167,105 +235,174 @@ pub fn photo_preview(props: &PhotoPreviewProps) -> Html {
         set_panning,
     );
 
-    let pan = {
+    // let pan = {
+    //     let photo_node = photo_node.clone();
+    //     let container_node = container_node.clone();
+
+    //     use_callback(
+    //         move |client_xy: (i32, i32), view_state| {
+    //             let mut vs = **view_state;
+    //             vs.pan(client_xy, &container_node, &photo_node);
+    //             view_state.set(vs);
+
+    //             // let (client_x, client_y) = client_xy;
+
+    //             // if view_state.is_panning {
+    //             //     let current = XY(client_x, client_y);
+    //             //     let mut offset = view_state.offset;
+
+    //             //     if let Some(prev) = view_state.previous {
+    //             //         let delta = current - prev;
+    //             //         offset += delta;
+
+    //             //         clamp_photo_offset(&mut offset, view_state.zoom, &container_node, &photo_node);
+    //             //     }
+
+    //             //     let mut new_state = **view_state;
+    //             //     new_state.offset = offset;
+    //             //     new_state.previous = Some(current);
+    //             //     view_state.set(new_state);
+    //             // }
+    //         },
+    //         view_state.clone(),
+    //     )
+    // };
+
+    // let pinch = {
+    //     let photo_node = photo_node.clone();
+
+    //     use_callback(
+    //         move |finger_distance: f64, view_state| {
+    //             let mut new_state = **view_state;
+
+    //             if let Some(prev_distance) = new_state.finger_distance {
+    //                 if let Some(photo_element) = photo_node.cast::<HtmlElement>() {
+    //                     let photo_width = photo_element.client_width() as f64;
+    //                     let delta = prev_distance - finger_distance;
+    //                     let zoom_step = (delta / photo_width) * 100.;
+    //                     let zoom_step = (view_state.zoom / 100.) * zoom_step;
+    //                     let new_zoom = view_state.zoom - zoom_step;
+    //                     let new_zoom = f64::clamp(new_zoom, ZOOM_FACTOR_MIN, ZOOM_FACTOR_MAX);
+    //                     new_state.zoom = new_zoom;
+    //                 }
+    //             }
+
+    //             new_state.finger_distance = Some(finger_distance);
+    //             view_state.set(new_state);
+    //         },
+    //         view_state.clone(),
+    //     )
+    // };
+
+    // let on_mouse_move = use_callback(
+    //     move |event: MouseEvent, pan| {
+    //         pan.emit((event.client_x(), event.client_y()));
+    //     },
+    //     pan.clone(),
+    // );
+
+    let on_mouse_move = {
         let photo_node = photo_node.clone();
         let container_node = container_node.clone();
 
         use_callback(
-            move |client_xy: (i32, i32), view_state| {
-                let (client_x, client_y) = client_xy;
-
-                if view_state.is_panning {
-                    let current = XY(client_x, client_y);
-                    let mut offset = view_state.offset;
-
-                    if let Some(prev) = view_state.previous {
-                        let delta = current - prev;
-                        offset += delta;
-
-                        clamp_photo_offset(&mut offset, view_state.zoom, &container_node, &photo_node);
-                    }
-
-                    let mut new_state = **view_state;
-                    new_state.offset = offset;
-                    new_state.previous = Some(current);
-                    view_state.set(new_state);
-                }
+            move |event: MouseEvent, view_state| {
+                let mut updated_state = **view_state;
+                updated_state.pan((event.client_x(), event.client_y()), &container_node, &photo_node);
+                view_state.set(updated_state);
             },
             view_state.clone(),
         )
     };
 
-    let pinch = {
+    // let on_touch_move = use_callback(
+    //     move |event: TouchEvent, (pan, pinch)| {
+    //         let n_touches = event.touches().length();
+    //         let mut touches = vec![];
+    //         for i in 0..n_touches {
+    //             if let Some(touch) = event.touches().item(i) {
+    //                 touches.push(touch);
+    //             }
+    //         }
+
+    //         {
+    //             // Handle panning
+    //             // Touch position is average X and Y of all touches
+
+    //             let touch_x = touches.iter().map(|t| t.client_x()).sum::<i32>() / n_touches as i32;
+    //             let touch_y = touches.iter().map(|t| t.client_y()).sum::<i32>() / n_touches as i32;
+
+    //             pan.emit((touch_x, touch_y));
+    //         }
+
+    //         {
+    //             // Handle pinch zooming
+    //             // Only take the first two touches into account for now
+
+    //             if n_touches >= 2 {
+    //                 let touch_1 = touches.get(0).unwrap();
+    //                 let touch_2 = touches.get(1).unwrap();
+    //                 let t1_x = touch_1.client_x() as f64;
+    //                 let t1_y = touch_1.client_y() as f64;
+    //                 let t2_x = touch_2.client_x() as f64;
+    //                 let t2_y = touch_2.client_y() as f64;
+
+    //                 let distance = ((t2_x - t1_x).powf(2.) + (t2_y - t1_y).powf(2.)).sqrt();
+    //                 pinch.emit(distance);
+    //             }
+    //         }
+    //     },
+    //     (pan, pinch),
+    // );
+
+    let on_touch_move = {
         let photo_node = photo_node.clone();
+        let container_node = container_node.clone();
 
         use_callback(
-            move |finger_distance: f64, view_state| {
-                let mut new_state = **view_state;
-
-                if let Some(prev_distance) = new_state.finger_distance {
-                    if let Some(photo_element) = photo_node.cast::<HtmlElement>() {
-                        let photo_width = photo_element.client_width() as f64;
-                        let delta = prev_distance - finger_distance;
-                        let zoom_step = (delta / photo_width) * 100.;
-                        let zoom_step = (view_state.zoom / 100.) * zoom_step;
-                        new_state.zoom -= zoom_step;
+            move |event: TouchEvent, view_state| {
+                let n_touches = event.touches().length();
+                let mut touches = vec![];
+                for i in 0..n_touches {
+                    if let Some(touch) = event.touches().item(i) {
+                        touches.push(touch);
                     }
                 }
 
-                new_state.finger_distance = Some(finger_distance);
-                view_state.set(new_state);
+                let mut updated_state = **view_state;
+
+                {
+                    // Handle panning
+                    // Touch position is average X and Y of all touches
+
+                    let touch_x = touches.iter().map(|t| t.client_x()).sum::<i32>() / n_touches as i32;
+                    let touch_y = touches.iter().map(|t| t.client_y()).sum::<i32>() / n_touches as i32;
+
+                    updated_state.pan((touch_x, touch_y), &container_node, &photo_node);
+                }
+
+                {
+                    // Handle pinch zooming
+                    // Only take the first two touches into account for now
+
+                    if n_touches >= 2 {
+                        let touch_1 = touches.get(0).unwrap();
+                        let touch_2 = touches.get(1).unwrap();
+                        let t1_x = touch_1.client_x() as f64;
+                        let t1_y = touch_1.client_y() as f64;
+                        let t2_x = touch_2.client_x() as f64;
+                        let t2_y = touch_2.client_y() as f64;
+
+                        let distance = ((t2_x - t1_x).powf(2.) + (t2_y - t1_y).powf(2.)).sqrt();
+                        updated_state.pinch(distance, &container_node, &photo_node);
+                    }
+                }
+
+                view_state.set(updated_state);
             },
             view_state.clone(),
         )
     };
-
-    let on_mouse_move = use_callback(
-        move |event: MouseEvent, pan| {
-            pan.emit((event.client_x(), event.client_y()));
-        },
-        pan.clone(),
-    );
-
-    let on_touch_move = use_callback(
-        move |event: TouchEvent, (pan, pinch)| {
-            let n_touches = event.touches().length();
-            let mut touches = vec![];
-            for i in 0..n_touches {
-                if let Some(touch) = event.touches().item(i) {
-                    touches.push(touch);
-                }
-            }
-
-            {
-                // Handle panning
-                // Touch position is average X and Y of all touches
-
-                let touch_x = touches.iter().map(|t| t.client_x()).sum::<i32>() / n_touches as i32;
-                let touch_y = touches.iter().map(|t| t.client_y()).sum::<i32>() / n_touches as i32;
-
-                pan.emit((touch_x, touch_y));
-            }
-
-            {
-                // Handle pinch zooming
-                // Only take the first two touches into account for now
-
-                if n_touches >= 2 {
-                    let touch_1 = touches.get(0).unwrap();
-                    let touch_2 = touches.get(1).unwrap();
-                    let t1_x = touch_1.client_x() as f64;
-                    let t1_y = touch_1.client_y() as f64;
-                    let t2_x = touch_2.client_x() as f64;
-                    let t2_y = touch_2.client_y() as f64;
-
-                    let distance = ((t2_x - t1_x).powf(2.) + (t2_y - t1_y).powf(2.)).sqrt();
-                    pinch.emit(distance);
-                }
-            }
-        },
-        (pan, pinch),
-    );
 
     let style = format!(
         "scale: {}; top: {}px; left: {}px",
