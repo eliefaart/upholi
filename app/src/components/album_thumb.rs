f12537e0ac04d16fe916dc12b6c5586e0be56b8b
Eric Liefaart <eric@e10t.nl>
2023-06-28 23:12:44 +0200

    Show shared state in albums view

--- app/src/components/album_thumb.rs
+++ app/src/components/album_thumb.rs
@@ -1,4 +1,8 @@
-use crate::{hooks::use_photo_src::use_photo_src, models::Album};
+use crate::{
+    components::IconShare,
+    hooks::{use_photo_src::use_photo_src, use_shares},
+    models::Album,
+};
 use upholi_lib::PhotoVariant;
 use yew::prelude::*;
 
@@ -9,17 +13,25 @@ pub struct AlbumThumbProps {
 
 #[function_component(AlbumThumb)]
 pub fn album_thumb(props: &AlbumThumbProps) -> Html {
+    let (shares, _) = use_shares();
     let src = use_photo_src(
         &props.album.thumbnail_photo_id.clone().unwrap_or_default(),
         PhotoVariant::Thumbnail,
     );
+
+    let is_shared = shares.iter().any(|share| share.album_id == props.album.id);
 
     html! {
         <div class={"album-thumb"}>
             <div
                 class={"img"}
                 style={format!("background-image: url({})", &(*src))}/>
-            <span class={"title"}>{&props.album.title}</span>
+            <div class={"title"}>
+                <span>{&props.album.title}</span>
+                if is_shared {
+                    <IconShare/>
+                }
+            </div>
         </div>
     }
 }
